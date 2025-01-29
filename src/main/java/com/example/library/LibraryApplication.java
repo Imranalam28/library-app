1d6aabcd09f128a46ba06741b707faf1ff6ee6d8
Imran Alam <alam.imran21510@gmail.com>
2025-01-29 18:04:40 +0530

    Initial commit for Heroku deployment 2

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,13 +1,24 @@
 package com.example.library;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
+
+	// Adding a basic controller mapping
+    @RestController
+    public static class LibraryController {
+
+        @GetMapping("/")
+        public String libraryHome() {
+            return "Welcome to the Library!";
+        }
+    }
 
 }
